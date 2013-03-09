96581d8af4f00fea82484eec2e82e59672c77674
yukihariguchi <yuki.hariguchi@gmail.com>
2013-03-09 20:02:57 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenCollege::Application.config.secret_token = '79c95cf2173971fb8091f392548fc2ad943dc0a0e80d55748a722601b525d3f077471f9177c107d26c9ebded1d9c878b69d8bd0cea1c25e1ac8d6daf499eae65'
