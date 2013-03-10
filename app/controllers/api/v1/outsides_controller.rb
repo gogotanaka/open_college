d6245c947fd5dfb0139bb5d8c6c0508e2f794a95
yukihariguchi <yuki.hariguchi@gmail.com>
2013-03-10 22:21:13 +0900

    allow domain access from keio

--- app/controllers/api/v1/outsides_controller.rb
+++ app/controllers/api/v1/outsides_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class OutsidesController < ApplicationController
 
-      before_filter :restrict_access
+      before_filter :allow_cross_domain_access, :restrict_access
 
       respond_to :json
 
@@ -16,6 +16,12 @@ module Api
         def restrict_access
           api_key = User.find_by_access_token(params[:access_token])
           head :unauthorized unless api_key
+        end
+
+        def allow_cross_domain_access
+          response.headers["Access-Control-Allow-Origin"] = "https://www2.adst.keio.ac.jp/rcs/servlet/jp.ac.keio.rcs.servlet.view.TopMenuServlet"
+          response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+          response.headers["Access-Control-Allow-Methods"] = "PUT,DELETE,POST,GET,OPTIONS"
         end
 
     end
