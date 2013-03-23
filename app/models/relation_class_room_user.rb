45253d3eaf772ae037c0ed9361fca9cc27be8ecb
tanaka <yakuruto771@live.jp>
2013-03-23 17:00:13 +0900

    fix

--- app/models/relation_class_room_user.rb
+++
@@ -1,8 +0,0 @@
-class RelationClassRoomUser < ActiveRecord::Base
-  attr_accessible :class_room_id, :user_id
-
-  belongs_to :class_room
-  belongs_to :user
-
-  
-end
--- test/unit/relation_class_room_user_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RelationClassRoomUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
