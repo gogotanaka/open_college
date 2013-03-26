89430842596e3cf51192697bdbab6415dcf0156d
Yuki Hariguchi <yuki.hariguchi@gmail.com>
2013-03-26 15:27:17 +0900

    add mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+
+  before_filter :prepare_for_mobile
+
+  private
+    def mobile_device?
+      request.user_agent =~ /Mobile|webOS/
+    end
+    helper_method :mobile_device?
+
+    def prepare_for_mobile
+      request.format = :mobile if mobile_device?
+    end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,15 @@
 class WelcomeController < ApplicationController
+  
   def index
     if current_user 
       redirect_to user_path(current_user)
       return 
     end
     @user = User.new
+    respond_to do |format|
+      format.html
+      format.mobile
+    end
   end
 
 end
