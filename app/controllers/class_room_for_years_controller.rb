8b8f3303acd59f69ec3a469bab2fd82bb11361d9
Yuki Hariguchi <yuki.hariguchi@gmail.com>
2013-03-30 19:22:35 +0900

    autocomplete

--- app/controllers/class_room_for_years_controller.rb
+++ app/controllers/class_room_for_years_controller.rb
@@ -1,5 +1,9 @@
 class ClassRoomForYearsController < ApplicationController
 
+  def index
+    render json: ClassRoomForYear.terms_for(params[:term])
+  end
+
   def show
     @class_room_for_year = ClassRoomForYear.find(params[:id])
   end
