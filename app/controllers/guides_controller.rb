a9f87676f6cf7875689bfef17c16772c5c6ff784
yukihariguchi <yuki.hariguchi@gmail.com>
2013-04-02 19:08:05 +0900

    mobile guide

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -19,15 +19,21 @@ class GuidesController < ApplicationController
   end
 
   def finish
-  	@user = current_user
-    @rank_all_in_university = ClassGrade.select('user_id, 1.0 * sum(grade)/count(grade) gpa').group('user_id').to_a.map{|x|sprintf( "%.2f", x.gpa )}.sort{|a, b| b <=> a}.index(@user.calculate) + 1
-    @rank_all_in_department = @user.department.class_grades.select('user_id, 1.0 * sum(grade)/count(grade) gpa').group('user_id').to_a.map{|x|sprintf( "%.2f", x.gpa )}.sort{|a, b| b <=> a}.index(@user.calculate) + 1
-    @rank_all_in_school_subject = @user.school_subject.class_grades.select('user_id, 1.0 * sum(grade)/count(grade) gpa').group('user_id').to_a.map{|x|sprintf( "%.2f", x.gpa )}.sort{|a, b| b <=> a}.index(@user.calculate) + 1
-    @page_title = "学業成績・総合"
-    @meta_description = "大学の授業の新しい形、OpenCollege。"
-    respond_to do |format|
-      format.html
-      format.mobile
+    @user = User.find(params[:id])
+    if @user.university
+    	@user = current_user
+      @rank_all_in_university = ClassGrade.select('user_id, 1.0 * sum(grade)/count(grade) gpa').group('user_id').to_a.map{|x|sprintf( "%.2f", x.gpa )}.sort{|a, b| b <=> a}.index(@user.calculate) + 1
+      @rank_all_in_department = @user.department.class_grades.select('user_id, 1.0 * sum(grade)/count(grade) gpa').group('user_id').to_a.map{|x|sprintf( "%.2f", x.gpa )}.sort{|a, b| b <=> a}.index(@user.calculate) + 1
+      @rank_all_in_school_subject = @user.school_subject.class_grades.select('user_id, 1.0 * sum(grade)/count(grade) gpa').group('user_id').to_a.map{|x|sprintf( "%.2f", x.gpa )}.sort{|a, b| b <=> a}.index(@user.calculate) + 1
+      @page_title = "学業成績・総合"
+      @meta_description = "大学の授業の新しい形、OpenCollege。"
+      respond_to do |format|
+        format.html
+        format.mobile
+      end
+    else
+      flash[:error] = '成績の解析が完了していません。もう一度誘導にしたがってやり直してください。解析が完了したら、OpenCollegeを使うことができます。解析したデータは匿名で、大学の授業をオープンにすること、あなたの授業選びのツール、のみに使われます。'
+      redirect_to intro_guide_url(@user)
     end
   end
 
