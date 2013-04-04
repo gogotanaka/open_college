eb52db4c2f54834baf901df8cccc6743e9b99b89
Yuki Hariguchi <yuki.hariguchi@gmail.com>
2013-04-04 16:23:25 +0900

    teacher mobile and menu selected

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,9 @@
 class TeachersController < ApplicationController
   def show
     @teacher = Teacher.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.mobile
+    end
   end
 end
