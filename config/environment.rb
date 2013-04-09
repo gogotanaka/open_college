e9ba23d1bb8958d06e820f5c83122589b2031754
Yuki Hariguchi <yuki.hariguchi@gmail.com>
2013-04-09 17:49:22 +0900

    from add

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 OpenCollege::Application.initialize!
 
+config.action_mailer.delivery_method = :smtp
+
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
