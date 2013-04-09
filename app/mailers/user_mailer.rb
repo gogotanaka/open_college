73133d1858081e468d088a137645ba3b477cc569
yukihariguchi <yuki.hariguchi@gmail.com>
2013-04-09 18:24:33 +0900

    send mail by gmail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,13 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "data.opencollege@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.signup_confirmation.subject
   #
-  def 
+  def signup_confirmation
     @greeting = "Hi"
-
     mail to: "yuki.hariguchi@gmail.com", subject: "Sign Up Confirmation"
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,19 @@ OpenCollege::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "heroku.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "data.opencollege@gmail.com",
+    password: "0401data"
+  }
 end
