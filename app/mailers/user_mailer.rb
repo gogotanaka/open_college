65067236fbed23fda65d88535a650b3ed16e14fb
Yuki Hariguchi <yuki.hariguchi@gmail.com>
2013-04-09 17:20:49 +0900

    from add

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < ActionMailer::Base
+  default from: "OpenCollege open-college.herokuapp.com/"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
